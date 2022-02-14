424ea232a6777e18fae1b57a9028fd63eae7d84a
unknown <gabriel,maia1333@gmail.com>
2022-02-14 18:41:10 -0300

    Projeto - React

---
+++ Projeto React/blogpessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,18 @@
+import react from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className='titulo'>
+                Home
+            </h1>
+            <img src= "https://t3.ftcdn.net/jpg/02/93/53/16/360_F_293531639_RTec7iO5kBTQRGDYQBXdaoWQY9hY2KpJ.jpg"
+            alt = "ILogo" className='img'/>
+
+        </>
+    );
+}
+
+
+export default Home;
