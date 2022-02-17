4eff1ca1eec3f5ae783fb260a60eac5c76317db6
unknown <gabriel,maia1333@gmail.com>
2022-02-17 19:24:47 -0300

    Atualizando o projeto

--- Projeto React/blogpessoal/src/App.tsx
+++ Projeto React/blogpessoal/src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Nav';
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
 import './App.css';
-import Home from './paginas/home/Home'
+import Login from './paginas/login/Login'
+
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Navbar />
+        <Switch>
+          <div style={{minHeight: '100vh'}}>
+
+            <Route exact path='/'>
+              <Login />
+            </Route>
+
+            <Route path='/login'>
+              <Login />
+            </Route>
+
+            <Route path='/home'>
+              <Home />
+            </Route>
+          </div>
+        </Switch>
+      <Footer />
+    </Router>
   );
 }
-
 export default App;
--- Projeto React/blogpessoal/src/components/estaticos/navbar/Nav.tsx
+++ Projeto React/blogpessoal/src/components/estaticos/navbar/Nav.tsx
@@ -1,46 +1,55 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { Link } from 'react-router-dom'
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';   
+import './Navbar.css'
 function Navbar() {
     return (
         <>
             <AppBar position="static">
+            <Box style={{ backgroundColor: "darkblue" }}>
                 <Toolbar variant="dense">
-                    <Box style={{ cursor: "pointer" }} >
+                    <Box mx={5}  className= "cursor" >
                         <Typography variant="h5" color="inherit">
-                            BlogPessoal
+                            Blog Pessoal   
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={2} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                home
+                                Home
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={2} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                postagens
+                                Postagens
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={2} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                temas
+                                Temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={2} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                cadastrar tema
+                                Cadastrar tema
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
-                            <Typography variant="h6" color="inherit">
-                                logout
-                            </Typography>
-                        </Box>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={2} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Logout
+                                </Typography>
+                            </Box>
+                        
+                        
+                        </Link>
                     </Box>
-
+                    
                 </Toolbar>
+                </Box>
             </AppBar>
+        
         </>
     )
 }
